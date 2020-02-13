0256345ab207e265cb3d8052325531d1140d00d7
pravesh1625 <55150407+pravesh1625@users.noreply.github.com>
2020-02-13 20:45:17 +0530

    Codechef problem School of Geometry  FEB 20B
    Codechef problem School of Geometry  FEB 20B

---
+++ ca.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+	for(int k=0;k<t;k++)
+	{
+		int n;
+		int long long count=0;
+		cin>>n;
+		int A[n];
+		int B[n];
+		for(int i=0;i<n;i++)
+		{
+			cin>>A[i];
+		}
+		for(int i=0;i<n;i++)
+		{
+			cin>>B[i];
+		}
+		sort(A,A+n);
+		sort(B,B+n);
+		for(int i=0;i<n;i++)
+		{
+			count=count + min(A[i],B[i]);
+		}
+		cout<<count<<endl;
+	}
+	
+	
+	return(0);
+}
